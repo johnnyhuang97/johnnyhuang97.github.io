b31f1725519f38d5e7ca2ce149978f8918526b88
johnnyhuang97 <johnny.h1997@berkeley.edu>
2020-07-29 23:54:58 -0700

    js update

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@
     var TxtRotate = function(el, toRotate, period) {
         this.toRotate = toRotate;
         this.el = el;
-        this.loopNum = loopNum;
+        this.loopNum = 0;
         this.period = parseInt(period, 10) || 2000;
         this.txt = '';
         this.tick();
@@ -41,12 +41,12 @@
             this.txt = fullTxt.substring(0, this.txt.length + 1);
         }
 
-        this.el.innerHTML = '<span class="wrap">'+ this.txt + '</span>';
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
         var that = this;
         var delta = 300 - Math.random() * 100;
 
-        if (this.isDeleting) { delta /= 2;}
+        if (this.isDeleting) { delta /= 2; }
 
         if (!this.isDeleting && this.txt === fullTxt) {
             delta = this.period;
@@ -71,6 +71,11 @@
                 new TxtRotate(elements[i], JSON.parse(toRotate), period);
             }
         }
+
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+        document.body.appendChild(css);
     };
 
    /* preloader
